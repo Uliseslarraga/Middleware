6ef73e93e0c7e52d5ac08ae0d8ef9490f9f2b6d3
Ulises Larraga <uliseslarraga@gmail.com>
2014-10-19 17:09:56 -0500

    tercer commit ulises

--- index.php
+++ index.php
@@ -5,14 +5,45 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+$capsule->addConnection(array(
+  'driver'    => 'pgsql',
+  'host'      => 'localhost',
+  'database'  => 'task',
+  'username'  => 'postgres',
+  'password'  => '',
+  'prefix'    => '',
+  'charset'   => 'utf8',
+  'collation' => 'utf8_general_ci',
+));
+$capsule->bootEloquent();
+$capsule->setAsGlobal();
+$app->db = $capsule->connection();
+
 $app->config(array(
  'debug' => 'true',
  'templates.path' => 'views',
  'view' => new \Slim\Views\Twig()
 	));
 
+$view = $app->view();
+$app->view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+    new Twig_Extension_Debug()
+);
+
 $app->get('/', function() use($app){
 	$app->render('home.twig');
 })->name('root');
+
+$app->post('/register-task', function() use($app){
+	$post = (object) $app->request->post();
+	$task = new Tasks();
+	$task->task=$post->$task;
+	$task->save();
+	$app->flash('error','Usuario registrado con éxito!');
+	$app->flashKeep();
+});
 
 $app->run();
---
+++ views/models/Tasks.php
@@ -0,0 +1,5 @@
+<?php
+class Tasks extends Illuminate\Database\Eloquent\Model{
+	protected $table = 'tasks';
+	public $timestamps = false;
+}
